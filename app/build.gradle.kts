71d5e85d73603899d0bd1a9c69407f18807ca892
1552980358 <1552980358@qq.com>
2024-08-05 22:30:26 +0800

    app: Implement module `material-symbols-annotation` & `material-symbols-ksp`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,9 @@ android {
 }
 
 dependencies {
+    implementation(project(":material-symbols-annotation"))
+    ksp(project(":material-symbols-ksp"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
