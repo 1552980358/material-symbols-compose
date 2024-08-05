a059d62a188a1db91c55da6b53764da024e3111c
1552980358 <1552980358@qq.com>
2024-08-05 21:58:41 +0800

    material-symbols-ksp: Update Coder

---
+++ material-symbols-ksp/src/main/java/me/ks/chan/material/symbols/ksp/coder/Coder.kt
@@ -0,0 +1,20 @@
+package me.ks.chan.material.symbols.ksp.coder
+
+import com.google.devtools.ksp.processing.CodeGenerator
+import com.google.devtools.ksp.processing.Dependencies
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.ksp.writeTo
+
+interface Coder {
+
+    val fileSpec: FileSpec
+
+    val dependencies: Dependencies
+        get() = Dependencies(aggregating = false)
+
+}
+
+fun CodeGenerator.generateWith(coder: Coder) {
+    coder.fileSpec
+        .writeTo(codeGenerator = this, coder.dependencies)
+}
