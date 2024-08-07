92e4048d00297821864ec031cbf88fe62db73824
1552980358 <1552980358@qq.com>
2024-08-07 21:51:32 +0800

    material-symbols-ksp: Update PathBuilderRepository

---
+++ material-symbols-ksp/src/main/java/me/ks/chan/material/symbols/ksp/repository/PathBuilderRepository.kt
@@ -0,0 +1,55 @@
+package me.ks.chan.material.symbols.ksp.repository
+
+sealed class PathBuilderCommand(
+    val command: String,
+    val floatParameters: FloatArray
+) {
+    class LineTo(floatParameterList: List<Float>): PathBuilderCommand(
+        command = "lineTo", floatParameters = floatParameterList.toFloatArray()
+    )
+    class MoveTo(floatParameterList: List<Float>): PathBuilderCommand(
+        command = "moveTo", floatParameters = floatParameterList.toFloatArray()
+    )
+    class QuadTo(floatParameterList: List<Float>): PathBuilderCommand(
+        command = "quadTo", floatParameters = floatParameterList.toFloatArray()
+    )
+    data object Close : PathBuilderCommand(
+        command = "close", floatParameters = floatArrayOf()
+    )
+}
+
+class PathBuilderRepository {
+
+    private enum class VectorDrawablePathCommand(val command: Char) {
+        MoveTo('M'),
+        LineTo('L'),
+        QuadTo('Q'),
+        Close('Z'),;
+
+        companion object {
+            operator fun get(command: Char) = VectorDrawablePathCommand.entries
+                .find { it.command == command }
+        }
+
+        fun pathBuilderCommand(parameters: String): PathBuilderCommand {
+            val floatParameters = parameters.substring(1)
+                .split(',', ' ')
+                .filter(String::isNotBlank)
+                .map(String::toFloat)
+
+            return when (this) {
+                MoveTo -> PathBuilderCommand.MoveTo(floatParameters)
+                LineTo -> PathBuilderCommand.LineTo(floatParameters)
+                QuadTo -> PathBuilderCommand.QuadTo(floatParameters)
+                Close -> PathBuilderCommand.Close
+            }
+        }
+    }
+
+    fun process(pathCommandList: List<String>): List<PathBuilderCommand> {
+        return pathCommandList.mapNotNull { pathCommand ->
+            VectorDrawablePathCommand[pathCommand.first()]?.pathBuilderCommand(pathCommand)
+        }
+    }
+
+}
