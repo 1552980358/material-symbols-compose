114c2e804dc79424c95d0219ef659e5d56e49914
1552980358 <1552980358@qq.com>
2024-08-08 09:13:18 +0800

    material-symbols-ksp: Set GoogleMaterialSymbolsRepository.repositoryUrl as public

--- material-symbols-ksp/src/main/java/me/ks/chan/material/symbols/ksp/repository/GoogleMaterialSymbolsRepository.kt
+++ material-symbols-ksp/src/main/java/me/ks/chan/material/symbols/ksp/repository/GoogleMaterialSymbolsRepository.kt
@@ -1,6 +1,5 @@
 package me.ks.chan.material.symbols.ksp.repository
 
-import com.google.devtools.ksp.processing.KSPLogger
 import me.ks.chan.material.symbols.annotation.MaterialSymbolGrade
 import me.ks.chan.material.symbols.annotation.MaterialSymbolWeight
 import me.ks.chan.material.symbols.ksp.annotation.MaterialSymbolIcon
@@ -9,7 +8,6 @@ import me.ks.chan.material.symbols.ksp.annotation.gradeUrlOption
 import me.ks.chan.material.symbols.ksp.annotation.opticalSizeInt
 import me.ks.chan.material.symbols.ksp.annotation.styleUrlOption
 import me.ks.chan.material.symbols.ksp.annotation.weightUrlOption
-import me.ks.chan.material.symbols.ksp.ext.asPascalCase
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
@@ -19,7 +17,7 @@ class GoogleMaterialSymbolsRepository(
 ) {
 
     @Suppress("SpellCheckingInspection")
-    private val repositoryUrl by lazy {
+    val repositoryUrl by lazy {
         "https://github.com/google/material-design-icons/blob/master/symbols/android/" +
             icon + "/materialsymbols" + materialSymbolIcon.styleUrlOption + "/" +
             icon + materialSymbolIcon.customizationOption + "_" +
