2e539f7a6157db4b557c724c679a7b89e4d2f78f
1552980358 <1552980358@qq.com>
2024-08-08 13:37:55 +0800

    material-symbols-ksp: Remove public MaterialSymbolIcon url string options

--- material-symbols-ksp/src/main/java/me/ks/chan/material/symbols/ksp/repository/GoogleMaterialSymbolsRepository.kt
+++ material-symbols-ksp/src/main/java/me/ks/chan/material/symbols/ksp/repository/GoogleMaterialSymbolsRepository.kt
@@ -1,13 +1,10 @@
 package me.ks.chan.material.symbols.ksp.repository
 
 import me.ks.chan.material.symbols.annotation.MaterialSymbolGrade
+import me.ks.chan.material.symbols.annotation.MaterialSymbolOpticalSize
+import me.ks.chan.material.symbols.annotation.MaterialSymbolStyle
 import me.ks.chan.material.symbols.annotation.MaterialSymbolWeight
 import me.ks.chan.material.symbols.ksp.annotation.MaterialSymbolIcon
-import me.ks.chan.material.symbols.ksp.annotation.filledUrlOption
-import me.ks.chan.material.symbols.ksp.annotation.gradeUrlOption
-import me.ks.chan.material.symbols.ksp.annotation.opticalSizeInt
-import me.ks.chan.material.symbols.ksp.annotation.styleUrlOption
-import me.ks.chan.material.symbols.ksp.annotation.weightUrlOption
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
@@ -20,8 +17,8 @@ class GoogleMaterialSymbolsRepository(
     val repositoryUrl by lazy {
         "https://github.com/google/material-design-icons/blob/master/symbols/android/" +
             icon + "/materialsymbols" + materialSymbolIcon.styleUrlOption + "/" +
-            icon + materialSymbolIcon.customizationOption + "_" +
-            "${materialSymbolIcon.opticalSizeInt}px.xml"
+            icon + materialSymbolIcon.customizationOption +
+            "_${materialSymbolIcon.opticalSizeInt}px.xml"
     }
 
     private val repositoryRequest: Request
@@ -44,4 +41,40 @@ private val MaterialSymbolIcon.customizationOption: String
             grade == MaterialSymbolGrade.Regular &&
             !filled -> { "" }
         else -> { "_${weightUrlOption}${gradeUrlOption}${filledUrlOption}" }
+    }
+
+private inline val MaterialSymbolIcon.filledUrlOption: String
+    get() = if (filled) "fill1" else ""
+
+private inline val MaterialSymbolIcon.styleUrlOption: String
+    get() = when (style) {
+        MaterialSymbolStyle.Outlined -> "outlined"
+        MaterialSymbolStyle.Rounded -> "rounded"
+        MaterialSymbolStyle.Sharp -> "sharp"
+    }
+
+private inline val MaterialSymbolIcon.weightUrlOption: String
+    get() = when (weight) {
+        MaterialSymbolWeight.Thinnest -> "wght100"
+        MaterialSymbolWeight.Thin -> "wght200"
+        MaterialSymbolWeight.Thinner -> "wght300"
+        MaterialSymbolWeight.Regular -> ""
+        MaterialSymbolWeight.Bold -> "wght500"
+        MaterialSymbolWeight.Bolder -> "wght600"
+        MaterialSymbolWeight.Boldest -> "wght700"
+    }
+
+private inline val MaterialSymbolIcon.gradeUrlOption: String
+    get() = when (grade) {
+        MaterialSymbolGrade.Low -> "gradN25"
+        MaterialSymbolGrade.Regular -> ""
+        MaterialSymbolGrade.High -> "grad200"
+    }
+
+private inline val MaterialSymbolIcon.opticalSizeInt: Int
+    get() = when (opticalSize) {
+        MaterialSymbolOpticalSize.Small -> 20
+        MaterialSymbolOpticalSize.Regular -> 24
+        MaterialSymbolOpticalSize.Large -> 40
+        MaterialSymbolOpticalSize.Larger -> 48
     }
